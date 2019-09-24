39b73f51924f61aee934a518a9227dee0278381e
Michail Roditis <ylovits@gmail.com>
2019-09-24 17:02:02 +0200

    Get from github api
    Installed axios, made get request for users from the github api. removed dummy users from Users component state added them as props.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { loading, users } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
