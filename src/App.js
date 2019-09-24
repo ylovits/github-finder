1fff31674e23cf47d2298270613c61e8b7a79cee
Michail Roditis <ylovits@gmail.com>
2019-09-24 18:20:23 +0200

    Search Component
    Implemented search using the github api to search users. Passing function as props for the search form sumbitting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
 import axios from "axios";
 import "./App.css";
@@ -10,13 +11,12 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  searchUsers = async text => {
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     const { loading, users } = this.state;
@@ -24,6 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={loading} users={users} />
         </div>
       </div>
