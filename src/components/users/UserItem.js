b0dbab54cdfa45cb5ed3b2f2087fcac2a99c7f43
Michail Roditis <ylovits@gmail.com>
2019-09-24 16:22:31 +0200

    User Page init
    Created basic user page with a little styling. Passed neew uers array dummy as props.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo"
-  };
-
   render() {
-    const { avatar_url, login, html_url } = this.state;
+    const { avatar_url, login, html_url } = this.props.user;
 
     return (
       <div className="card text-center">
